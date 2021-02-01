c899fe0dc4581a544cc6a4d06cc2bb2765d09979
Mohammed Farish <farishmusthafa@gmail.com>
2021-02-01 23:41:11 +0400

    minor fix

--- src/components/startpaw.js
+++ src/components/startpaw.js
@@ -75,7 +75,7 @@ class startPWA extends React.Component {
                                 latitude: this.state.latitude,
                                 accuracy: this.state.accuracy
                             };
-                            axios.post('http://192.168.1.18:5000/session', details)
+                            axios.post('https://dubatravels.herokuapp.com/session', details)
                                 .then(response => {
 
                                     const { session_token } = response.data
